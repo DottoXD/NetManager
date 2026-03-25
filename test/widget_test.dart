5fbe2ea030b3801fa1835aa0c94b99986d717865
DottoXD <80960175+DottoXD@users.noreply.github.com>
2026-03-25 09:35:27 +0100

    No more tests at least for now

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:netmanager/main.dart';
 
 void main() {
   testWidgets('Temp', (WidgetTester tester) async {
-    await tester.pumpWidget(const NetManager());
+    /*await tester.pumpWidget(const NetManager());
 
-    /*expect(find.text('0'), findsOneWidget);
+    expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     await tester.tap(find.byIcon(Icons.add));
